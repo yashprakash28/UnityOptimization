89a8423287f8745556166c86076ecc26051a9811
Yash Gupta <yashprakash28@users.noreply.github.com>
2021-01-07 03:22:15 +0530

    Trying to capture screen for finite number of times

---
+++ Optimize/Assets/Scripts/MainScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using TMPro;
+// using Firebase.Storage;
+using System.Threading;
+
+public class MainScript : MonoBehaviour
+{
+    int numberOfCaptures = 10;
+
+    bool status = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(startCapturing());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // start fetching data as soon as application opens
+        Debug.Log("update");
+
+        if(status)  StartCoroutine(captureImage());
+        
+    }
+
+    IEnumerator captureImage()
+    {
+        Debug.Log("enum");
+
+        // imageNumber++;
+
+        // clickStatus.text = "button clicked and went into enum";
+        yield return new WaitForEndOfFrame();
+        // clickStatus.text = "..";
+
+        Texture2D tex = ScreenCapture.CaptureScreenshotAsTexture();
+
+        byte[] bytes = tex.EncodeToPNG();
+        Object.Destroy(tex);
+
+        string path = Application.persistentDataPath + "/Image" + System.DateTime.Now.ToString("HHmmss") + ".png";
+        // string path = "C:/Users/YasHarsh/Desktop/Imagenow.png";
+        Debug.Log(path + " captured");
+        // int byteToInt = System.BitConverter.ToInt32(bytes, 0);
+        // Debug.Log(byteToInt);
+
+        // createImageArray(byteToInt, imageNumber);
+
+        // afterMath.text = path + " captured";
+        File.WriteAllBytes(path, bytes);
+        status = false;
+    }
+}
