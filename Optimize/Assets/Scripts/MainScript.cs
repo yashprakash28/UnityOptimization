1b15dc3c1f990ca9beafd727ab309a157b77745c
Yash Gupta <yashprakash28@users.noreply.github.com>
2021-01-09 04:03:06 +0530

    API Fetching initiated

--- Optimize/Assets/Scripts/MainScript.cs
+++ Optimize/Assets/Scripts/MainScript.cs
@@ -9,12 +9,13 @@ using System.Threading;
 public class MainScript : MonoBehaviour
 {
     int numberOfCaptures = 10;
+    string url = "www.google.com";
 
     bool status = true;
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(startCapturing());
+        // StartCoroutine(startCapturing());
     }
 
     // Update is called once per frame
@@ -23,35 +24,51 @@ public class MainScript : MonoBehaviour
         // start fetching data as soon as application opens
         Debug.Log("update");
 
-        if(status)  StartCoroutine(captureImage());
+        if(status)  request();
         
     }
 
-    IEnumerator captureImage()
+    public void request()
     {
-        Debug.Log("enum");
-
-        // imageNumber++;
-
-        // clickStatus.text = "button clicked and went into enum";
-        yield return new WaitForEndOfFrame();
-        // clickStatus.text = "..";
-
-        Texture2D tex = ScreenCapture.CaptureScreenshotAsTexture();
-
-        byte[] bytes = tex.EncodeToPNG();
-        Object.Destroy(tex);
-
-        string path = Application.persistentDataPath + "/Image" + System.DateTime.Now.ToString("HHmmss") + ".png";
-        // string path = "C:/Users/YasHarsh/Desktop/Imagenow.png";
-        Debug.Log(path + " captured");
-        // int byteToInt = System.BitConverter.ToInt32(bytes, 0);
-        // Debug.Log(byteToInt);
-
-        // createImageArray(byteToInt, imageNumber);
-
-        // afterMath.text = path + " captured";
-        File.WriteAllBytes(path, bytes);
         status = false;
+        Debug.Log("request");
+        WWW request = new WWW(url);
+        StartCoroutine(onResponse(request));
     }
+
+    private IEnumerator onResponse(WWW req)
+    {
+        yield return req;
+
+        Debug.Log(req.text);
+        status = true;
+    }
+
+    // IEnumerator captureImage()
+    // {
+    //     Debug.Log("enum");
+
+    //     // imageNumber++;
+
+    //     // clickStatus.text = "button clicked and went into enum";
+    //     yield return new WaitForEndOfFrame();
+    //     // clickStatus.text = "..";
+
+    //     Texture2D tex = ScreenCapture.CaptureScreenshotAsTexture();
+
+    //     byte[] bytes = tex.EncodeToPNG();
+    //     Object.Destroy(tex);
+
+    //     string path = Application.persistentDataPath + "/Image" + System.DateTime.Now.ToString("HHmmss") + ".png";
+    //     // string path = "C:/Users/YasHarsh/Desktop/Imagenow.png";
+    //     Debug.Log(path + " captured");
+    //     // int byteToInt = System.BitConverter.ToInt32(bytes, 0);
+    //     // Debug.Log(byteToInt);
+
+    //     // createImageArray(byteToInt, imageNumber);
+
+    //     // afterMath.text = path + " captured";
+    //     File.WriteAllBytes(path, bytes);
+    //     status = false;
+    // }
 }
