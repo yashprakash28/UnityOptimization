56a6964d91bb59146476a6b2cb47d1f31d94db6e
Yash Gupta <yashprakash28@users.noreply.github.com>
2021-01-16 02:39:44 +0530

    Testing getcoordinates api

--- Optimize/Assets/Scripts/MainScript.cs
+++ Optimize/Assets/Scripts/MainScript.cs
@@ -3,72 +3,97 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using TMPro;
-// using Firebase.Storage;
-using System.Threading;
+using UnityEngine.Networking;
 
 public class MainScript : MonoBehaviour
 {
-    int numberOfCaptures = 10;
-    string url = "www.google.com";
 
-    bool status = true;
-    // Start is called before the first frame update
+    string url = "http://ec2-3-139-61-182.us-east-2.compute.amazonaws.com:5000/getcoordinates";
+
+    static WebCamTexture camera;
+
     void Start()
     {
-        // StartCoroutine(startCapturing());
+        if (camera == null)
+        {
+            camera = new WebCamTexture();
+        }
+
+        GetComponent<Renderer>().material.mainTexture = camera;
+
+        if(!camera.isPlaying)
+        {
+            camera.Play();
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // start fetching data as soon as application opens
-        Debug.Log("update");
-
-        if(status)  request();
-        
+       
     }
 
-    public void request()
+    public void sendRequest()
     {
-        status = false;
+        StartCoroutine(captureImage());
+    }
+
+    IEnumerator request(Texture2D tex)
+    {
         Debug.Log("request");
-        WWW request = new WWW(url);
-        StartCoroutine(onResponse(request));
+        // WWW request = new WWW(url);
+        // StartCoroutine(onResponse(request));
+
+
+        byte[] bytes = tex.EncodeToJPG();
+        Destroy(tex);
+
+        var uwr = new UnityWebRequest(url, "POST");
+        uwr.uploadHandler = new UploadHandlerRaw(bytes);
+        uwr.downloadHandler = new DownloadHandlerBuffer();
+        uwr.SetRequestHeader("Content-Type", "image/jpeg");
+        // uwr.GetRequestHeader("image");
+
+        yield return uwr.SendWebRequest();
+
+        string response = uwr.downloadHandler.text;
+
+        Debug.Log(response);
+
+        yield return null;
+
     }
 
-    private IEnumerator onResponse(WWW req)
-    {
-        yield return req;
-
-        Debug.Log(req.text);
-        status = true;
-    }
-
-    // IEnumerator captureImage()
+    // private IEnumerator onResponse(WWW req)
     // {
-    //     Debug.Log("enum");
+    //     yield return req;
 
-    //     // imageNumber++;
+    //     Debug.Log(req.text);
 
-    //     // clickStatus.text = "button clicked and went into enum";
-    //     yield return new WaitForEndOfFrame();
-    //     // clickStatus.text = "..";
-
-    //     Texture2D tex = ScreenCapture.CaptureScreenshotAsTexture();
-
-    //     byte[] bytes = tex.EncodeToPNG();
-    //     Object.Destroy(tex);
-
-    //     string path = Application.persistentDataPath + "/Image" + System.DateTime.Now.ToString("HHmmss") + ".png";
-    //     // string path = "C:/Users/YasHarsh/Desktop/Imagenow.png";
-    //     Debug.Log(path + " captured");
-    //     // int byteToInt = System.BitConverter.ToInt32(bytes, 0);
-    //     // Debug.Log(byteToInt);
-
-    //     // createImageArray(byteToInt, imageNumber);
-
-    //     // afterMath.text = path + " captured";
-    //     File.WriteAllBytes(path, bytes);
-    //     status = false;
     // }
+
+    IEnumerator captureImage()
+    {
+        Debug.Log("enum");
+
+        yield return new WaitForEndOfFrame();
+
+        Texture2D tex = ScreenCapture.CaptureScreenshotAsTexture();
+
+        yield return StartCoroutine(request(tex));
+
+        // byte[] bytes = tex.EncodeToPNG();
+        // Object.Destroy(tex);
+
+        // string path = Application.persistentDataPath + "/Image" + System.DateTime.Now.ToString("HHmmss") + ".png";
+        // // string path = "C:/Users/YasHarsh/Desktop/Imagenow.png";
+        // Debug.Log(path + " captured");
+        // // int byteToInt = System.BitConverter.ToInt32(bytes, 0);
+        // // Debug.Log(byteToInt);
+
+        // // createImageArray(byteToInt, imageNumber);
+
+        // // afterMath.text = path + " captured";
+        // File.WriteAllBytes(path, bytes);
+        // status = false;
+    }
 }
